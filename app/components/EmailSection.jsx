88e9eb3db2a0798c79e13d74d43cb537f9f33b36
FVidalYepz <Frananyelvidalyepez@gmail.com>
2024-01-17 00:36:18 -0400

    Update EmailSection.jsx

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -53,9 +53,9 @@ const EmailSection = () => {
           Let&apos;s Connect
         </h5>
         <p className="text-[#ADB7BE] mb-4 w-full md:max-w-md text-justify border-t border-t-secondary-600">
-          I'm currently looking for new opportunities, my inbox is always open.
-          Whether you have a question or just want to say hi, I'll try my best
-          to get back to you!
+          I&apos;m currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&apos;ll
+          try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
           <Link
